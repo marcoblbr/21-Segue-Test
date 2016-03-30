9fdb0744c5dbe77e3b53d976eaf7071793a1dc67
marco <marcolinhares@Marcos-MacBook-Pro.local>
2016-03-30 17:59:27 -0300

    commit
    commit

---
+++ 21 - Segue Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  21 - Segue Test
+//
+//  Created by Marco on 7/21/15.
+//  Copyright (c) 2015 Marco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
